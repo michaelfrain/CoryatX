207bbb988a4cbd656b8d0b6bc8a3b0f34e70ec1b
Michael Frain <michaelfrain@michaels-imac.local>
2018-06-05 10:05:04 -0700

    Initial Commit

---
+++ CoryatX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoryatX
+//
+//  Created by Michael Frain on 6/5/18.
+//  Copyright © 2018 Michael Frain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
